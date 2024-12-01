0af5b205f637870a3c31d8732a0e365649ef0445
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-12-01 14:09:48 +0000

    Add event listener for submitting city query

--- src/getWeather.js
+++ src/getWeather.js
@@ -4,13 +4,9 @@ const apiKey =
   "?unitGroup=uk&include=current&key=AVW8NL5LTDLKH7TBJMKYDWJ53&contentType=json";
 
 const getWeather = async (city) => {
-  try {
-    const response = await fetch(`${baseURL}${city}${apiKey}`);
-    const weatherData = await response.json();
-    return weatherData.currentConditions;
-  } catch (err) {
-    console.error(err);
-  }
+  const response = await fetch(`${baseURL}${city}${apiKey}`);
+  const weatherData = await response.json();
+  return weatherData.currentConditions;
 };
 
 export { getWeather };
--- src/index.js
+++ src/index.js
@@ -6,7 +6,18 @@ let city = "Łomża";
 let cityFormatted = city.toLowerCase().split(" ").join("%20");
 console.log(cityFormatted);
 
-const data = await getWeather(cityFormatted);
-console.log(data);
-
-drawResults(city, data);
+const queryForm = document.querySelector(".query-form");
+const cityInput = document.querySelector("#city");
+queryForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  city = cityInput.value;
+  cityFormatted = city.toLowerCase().split(" ").join("%20");
+  getWeather(cityFormatted)
+    .then((data) => {
+      console.log(data);
+      drawResults(city, data);
+    })
+    .catch(() => {
+      console.log(new Error("This city doesn't exist"));
+    });
+});
