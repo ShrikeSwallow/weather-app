00661f24d00b33cf1278b7586961229c3e22c9cc
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-12-01 15:52:44 +0000

    Implement different styles for various weather conditions

--- src/index.js
+++ src/index.js
@@ -7,16 +7,22 @@ let city = "Łomża";
 let cityFormatted = city.toLowerCase().split(" ").join("%20");
 console.log(cityFormatted);
 
+const body = document.body;
+console.log(body);
 const queryForm = document.querySelector(".query-form");
 const cityInput = document.querySelector("#city");
 queryForm.addEventListener("submit", (event) => {
   event.preventDefault();
+  body?.setAttribute("class", "");
   city = cityInput.value;
   cityFormatted = city.toLowerCase().split(" ").join("%20");
   getWeather(cityFormatted)
     .then((data) => {
-      console.log(data);
+      console.log(data.conditions.toLowerCase().split(" ").join("-"));
       drawResults(city, data);
+      body.classList.add(
+        `${data.conditions.toLowerCase().split(" ").join("-")}`
+      );
     })
     .catch(() => {
       console.log(new Error("This city doesn't exist"));
