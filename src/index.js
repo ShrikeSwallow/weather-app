985b47eb8e43d14d660010ab43513d830d268b99
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-12-01 11:03:17 +0000

    Implement fetching data for a correct location

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./styles.css";
+
+const baseURL =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+const apiKey =
+  "?unitGroup=uk&include=current&key=AVW8NL5LTDLKH7TBJMKYDWJ53&contentType=json";
+
+const showData = (data) => {
+  console.log(data);
+};
+
+const getWeather = async () => {
+  let city = "london";
+  const response = await fetch(`${baseURL}${city}${apiKey}`);
+  const weatherData = await response.json();
+  return weatherData;
+};
+
+const data = await getWeather();
+console.log(data);
