2f106a64b20beeb3eb4824cd37263d2e93dad5bb
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-12-01 11:22:00 +0000

    Implement error handling and narrow down data to only needed bits

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ const baseURL =
 const apiKey =
   "?unitGroup=uk&include=current&key=AVW8NL5LTDLKH7TBJMKYDWJ53&contentType=json";
 
-const showData = (data) => {
-  console.log(data);
-};
+let city = "stratford-upon-avon";
+let cityFormatted = city.toLowerCase().split(" ").join("%20");
+console.log(cityFormatted);
 
 const getWeather = async () => {
-  let city = "london";
-  const response = await fetch(`${baseURL}${city}${apiKey}`);
-  const weatherData = await response.json();
-  return weatherData;
+  try {
+    const response = await fetch(`${baseURL}${cityFormatted}${apiKey}`);
+    const weatherData = await response.json();
+    return weatherData.currentConditions;
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const data = await getWeather();
