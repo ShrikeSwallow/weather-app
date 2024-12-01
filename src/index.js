29e3f54c6d4d21197dae7ec5f69bbd9d42624fdd
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-12-01 11:36:19 +0000

    Refactor move getWeather function into its own module

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 import "./styles.css";
+import { getWeather } from "./getWeather";
 
-const baseURL =
-  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
-const apiKey =
-  "?unitGroup=uk&include=current&key=AVW8NL5LTDLKH7TBJMKYDWJ53&contentType=json";
-
-let city = "stratford-upon-avon";
+let city = "Łomża";
 let cityFormatted = city.toLowerCase().split(" ").join("%20");
 console.log(cityFormatted);
 
-const getWeather = async () => {
-  try {
-    const response = await fetch(`${baseURL}${cityFormatted}${apiKey}`);
-    const weatherData = await response.json();
-    return weatherData.currentConditions;
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-const data = await getWeather();
+const data = await getWeather(cityFormatted);
 console.log(data);
